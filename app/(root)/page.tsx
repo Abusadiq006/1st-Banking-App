1800d34b269dd3a25b85e379ef5c0e714e35e464
Farmer Abusadiq <Abusadiq006@gmail.com>
2025-10-16 04:00:49 -0700

    created the amount, doughnut shape and the amount value with currency sign

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Muhammad' }
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and Manage your account and 
+            transactions efficiently.'
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
